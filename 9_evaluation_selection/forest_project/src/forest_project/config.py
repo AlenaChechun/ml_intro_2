16ee6b49764e27ca2af060bd3d45ddbb86df0b4d
AlenaChechun <achechun@gmail.com>
2022-05-09 23:25:40 +0300

    fixed for mypy

--- 9_evaluation_selection/forest_project/src/forest_project/config.py
+++ 9_evaluation_selection/forest_project/src/forest_project/config.py
@@ -7,22 +7,22 @@ Created on Thu May  5 18:57:41 2022
 
 import configparser
 
+from typing import List
 from pathlib import Path
 
 
 class Config:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._PREPROCESS_SECTION = 'preprocess'
-        self.path_ = None
         self.cfg = configparser.ConfigParser()
         self.drop_feature = None
 
-    def read_config(self, path : Path):
+    def read_config(self, path: Path) -> None:
         self.path_ = path
         self.cfg.read(path)
 
-    def get_drop_features(self):
+    def get_drop_features(self) -> List[str]:
         if self._PREPROCESS_SECTION in self.cfg.sections():
             drop_features = self.cfg[self._PREPROCESS_SECTION]['drop'].split('\n')
             return drop_features
